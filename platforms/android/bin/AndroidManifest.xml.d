c1488d84400851b7b1a3df4d9eac97a8c762d600
Carl Jamilkowski <cdj255@nyu.edu>
2013-12-02 19:17:00 -0500

    Android

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/carlj/Documents/Development/Android/workspace/echoonlocation/platforms/android/bin/AndroidManifest.xml : \
+/Users/carlj/Documents/Development/Android/workspace/echoonlocation/platforms/android/AndroidManifest.xml \
